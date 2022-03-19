5c8175edf5bbe854c411861ff67e4090c6072ba2
Koby Hale <42821995+Mr-HaleYa@users.noreply.github.com>
2022-03-18 22:34:41 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
- 
     setTimeout(function(){
         $('body').addClass('loaded');
-        $('h1').css('color','#222222');
+        $('h1').css('color','#222222'); // change text color back after preload
     }, 3000);
- 
 });

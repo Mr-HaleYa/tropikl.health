3abd99100026a8c98777311491d748e5da4d06ba
Koby Hale <42821995+Mr-HaleYa@users.noreply.github.com>
2022-03-22 11:07:11 -0600

    remove js timestamp

--- index.php
+++ index.php
@@ -65,18 +65,6 @@ echo '
 
     ?>
 
-<!-- <script defer>
-
-    var xReq = new XMLHttpRequest();
-    xReq.open("HEAD", "./scripts/fish.js", false);
-    xReq.send(null);
-    var lastModified = xReq.getResponseHeader("Last-Modified");
-    var myDate = new Date(lastModified);
-    var myEpoch = myDate.getTime()/1000.0;
-    document.write(myEpoch);
-
-</script> -->
-
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
